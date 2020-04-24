bcb969b26df8373df7921f4096ba30f626f2b300
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2020-04-24 20:33:34 +0530

    Check if rust and cargo exists before adding to path.

--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -1,3 +1,7 @@
-set -gx RUSTUP_HOME ~/.local/share/rustup
-set -gx CARGO_HOME ~/.local/share/cargo
-set -g fish_user_paths $CARGO_HOME/bin $fish_user_paths
+if command -sq rustup
+    set -gx RUSTUP_HOME ~/.local/share/rustup
+end
+if command -sq cargo
+    set -gx CARGO_HOME ~/.local/share/cargo
+    set -g fish_user_paths $CARGO_HOME/bin $fish_user_paths
+end
