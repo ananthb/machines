4f20dd4722cbbaeca95b990c642a7ec2a003d917
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2020-04-24 20:31:57 +0530

    Add cabal to path only if ghc exists.

--- .config/fish/conf.d/haskell.fish
+++ .config/fish/conf.d/haskell.fish
@@ -1,2 +1,4 @@
-set -gx GHCUP_INSTALL_BASE_PREFIX ~/.local/share
-set -g fish_user_paths $HOME/.cabal/bin $GHCUP_INSTALL_BASE_PREFIX/.ghcup/bin $fish_user_paths
+if command -sq ghci
+    set -gx GHCUP_INSTALL_BASE_PREFIX ~/.local/share
+    set -g fish_user_paths $HOME/.cabal/bin $GHCUP_INSTALL_BASE_PREFIX/.ghcup/bin $fish_user_paths
+end
