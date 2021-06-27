1244a19d6526a39c9100110ebdc1b2cb17a42bff
Ananth Bhaskararaman <antsub@gmail.com>
2021-06-27 16:06:46 +0530

    Added rustup and ghcup support.

---
+++ .config/fish/conf.d/ghcup.fish
@@ -0,0 +1,3 @@
+set -gx GHCUP_INSTALL_BASE_PREFIX $HOME/.local/share/ghcup
+set -gx GHCUP_USE_XDG_DIRS 1
+
---
+++ .config/fish/conf.d/rustup.fish
@@ -0,0 +1,3 @@
+set -gx RUSTUP_HOME ~/.local/share/rustup
+set -gx CARGO_HOME ~/.local/share/cargo
+fish_add_path $CARGO_HOME/bin
