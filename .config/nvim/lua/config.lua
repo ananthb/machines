a20d11775367d82d1f288a8b00d434e239011859
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2022-09-22 17:50:18 +0530

    More term futzing around

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,8 +1,17 @@
 -- NVIM TREE
--- disable netrw
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 require'nvim-tree'.setup()
+
+--NVIM TREESITTER
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "c", "cpp", "css", "dockerfile", "elm", "fish", "go", "gomod",
+    "haskell", "http", "javascript", "json", "lua", "make", "python",
+    "toml", "typescript", "yaml", "zig"
+  },
+  auto_install = true,
+}
 
 -- COQ
 local coq = require'coq'
