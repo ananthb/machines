3d386d3d360b063736fedbf0e6d64ee58c273fbb
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2022-09-24 20:09:19 +0530

    Add dashboard logo

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,3 +1,57 @@
+-- DASHBOARD
+local home = os.getenv('HOME')
+local db = require('dashboard')
+local sysname = vim.loop.os_uname().sysname
+if sysname == "Linux" then
+  -- linux
+  db.preview_command = 'ueberzug'
+elseif sysname == "Darwin" then
+  -- macos
+  db.preview_command = 'cat | lolcat -F 0.3'
+end
+
+db.preview_file_path = home .. '/.local/share/nvim/neovim.cat'
+db.preview_file_height = 11
+db.preview_file_width = 70
+db.custom_center = {
+  {
+    icon = '  ',
+    desc = 'Recently latest session                 ',
+    shortcut = 'SPC s l',
+    action ='SessionLoad'
+  },
+  {
+    icon = '  ',
+    desc = 'Recently opened files                   ',
+    action =  'DashboardFindHistory',
+    shortcut = 'SPC f h'
+  },
+  {
+    icon = '  ',
+    desc = 'Find  File                              ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'
+  },
+  {
+    icon = '  ',
+    desc ='File Browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'
+  },
+  {
+    icon = '  ',
+    desc = 'Find  word                              ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'
+  },
+  {
+    icon = '  ',
+    desc = 'Open Personal dotfiles                  ',
+    action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+    shortcut = 'SPC f d'
+  },
+}
+
 -- NVIM TREE
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
