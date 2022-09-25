721e806009fa201c301240cd0458097027aca364
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2022-09-25 21:53:46 +0530

    Fix config. LSPs and shizz.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,10 +7,16 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'                                              
 
-  -- display
+-- LSP Servers to install and setup
+LSPServers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua' }
+
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+
+  -- DISPLAY
   use 'tomasiser/vim-code-dark'
   use 'nvim-lua/popup.nvim'
   use {
@@ -20,19 +26,18 @@ require('packer').startup(function(use)
       require('lualine').setup {
         options = {
           icons_enabled = true,
-          theme = 'codedark',
-          component_separators = '|',
-          section_separators = '',
+          theme = 'auto',
         },
       }
     end
   }
+
   use {
     'glepnir/dashboard-nvim',
     config = function()
       local db = require('dashboard')
       db.preview_command = 'cat | lolcat'
-      db.preview_file_path = vim.fn.stdpath 'data' .. '/neovim.cat'
+      db.preview_file_path = vim.fn.stdpath 'config' .. '/neovim.cat'
       db.preview_file_height = 5
       db.preview_file_width = 55
       db.custom_center = {
@@ -70,17 +75,19 @@ require('packer').startup(function(use)
     end
   }
 
-  -- sessions
+
+  -- SESSIONS
   use {
     'rmagatti/auto-session',
     config = function()
-      require('auto-session').setup({
+      require('auto-session').setup{
         auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
         auto_session_enabled = true,
         auto_save_enabled = true,
         auto_restore_enabled = true,
-        auto_session_allowed_dirs = { "~/src/*" },
-      })
+        auto_session_supress_dirs = { '~' },
+        auto_session_allowed_dirs = { '~/src/*' },
+      }
     end
   }
   use {
@@ -91,7 +98,8 @@ require('packer').startup(function(use)
     end
   }
 
-  -- file tree
+
+  -- NVIM TREE
   use {
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
@@ -100,50 +108,116 @@ require('packer').startup(function(use)
     end
   }
 
-  -- completion
+
+  -- COQ
   use { 'ms-jpq/coq_nvim', branch = 'coq', requires = {'ms-jpq/coq.artifacts', branch = 'artifacts' } }
 
-  -- git
+
+  -- GIT
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
   use {
     'lewis6991/gitsigns.nvim',
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('gitsigns').setup {
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-      }
+      require('gitsigns').setup {}
     end
   }
 
-  -- treesitter
+
+  -- TREESITTER
   use {
     'nvim-treesitter/nvim-treesitter',
     requires = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     config = function()
       require'nvim-treesitter.configs'.setup {
         ensure_installed = {
-          "c", "cpp", "css", "dockerfile", "elm", "fish", "go", "gomod",
-          "haskell", "http", "javascript", "json", "lua", "make", "python",
-          "toml", "typescript", "yaml", "zig"
+          'c', 'cpp', 'css', 'dockerfile', 'elm', 'fish', 'go', 'gomod',
+          'haskell', 'javascript', 'json', 'lua', 'make', 'python',
+          'toml', 'typescript', 'yaml', 'zig'
         },
         auto_install = true,
       }
     end
   }
 
-  -- lsp
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
 
-  -- telescope
+  -- LSP
+  use {
+    'neovim/nvim-lspconfig',
+    config = function()
+
+      -- on_attach runs when an LSP attaches to a buffer
+      local on_attach = function(_, bufnr)
+        local nmap = function(keys, func, desc)
+          if desc then
+            desc = 'LSP: ' .. desc
+          end
+
+          vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+        end
+
+        nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+        nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+        nmap('gr', require('telescope.builtin').lsp_references)
+        nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        -- See `:help K` for why this keymap
+        nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+        nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+        -- Lesser used LSP functionality
+        nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+        nmap('<leader>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, '[W]orkspace [L]ist Folders')
+
+        -- Create a command `:Format` local to the LSP buffer
+        vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+          if vim.lsp.buf.format then
+            vim.lsp.buf.format()
+          elseif vim.lsp.buf.formatting then
+            vim.lsp.buf.formatting()
+          end
+        end, { desc = 'Format current buffer with LSP' })
+      end
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      -- init lsp servers
+      local lspconfig = require('lspconfig')
+      local cfg = { on_attach=on_attach, capabilities=capabilities }
+      for _, lsp in ipairs(LSPServers) do
+        if lsp == "sumneko_lua" then
+          cfg.settings = {
+            Lua = { diagnostics = { globals = { 'vim' } } }
+          }
+        end
+        lspconfig[lsp].setup(cfg)
+      end
+    end
+  }
+
+  use { 'williamboman/mason.nvim', config = function() require'mason'.setup{} end }
+
+  use {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require'mason-lspconfig'.setup {
+        ensure_installed = LSPServers
+      }
+    end
+  }
+
+
+  -- TELESCOPE
   use {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
@@ -161,10 +235,10 @@ require('packer').startup(function(use)
         },
       }
 
-      -- Enable telescope fzf native, if installed
+      -- enable telescope fzf native, if installed
       pcall(scope.load_extension, 'fzf')
 
-      -- See `:help telescope.builtin`
+      -- see `:help telescope.builtin`
       local telescope_builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
       vim.keymap.set('n', '<leader><space>', telescope_builtin.buffers, { desc = '[ ] Find existing buffers' })
@@ -184,27 +258,30 @@ require('packer').startup(function(use)
       vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, {desc = '[S]earch [B]uffers'})
     end
   }
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
 
-  -- editor
+  -- build telescope fzf native if make is available
+  use {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    run = 'make',
+    cond = vim.fn.executable 'make' == 1
+  }
+
+
+  -- EDITOR
   use 'ellisonleao/glow.nvim'
   use 'tpope/vim-sleuth'
   use 'kylechui/nvim-surround'
   use 'mhartington/formatter.nvim'
 
-  -- python envs
+
+  -- POETRY ENVS
   use 'petobens/poet-v'
+
 
   if is_bootstrap then
     require('packer').sync()
   end
 end)
-
-if is_bootstrap then
-  -- do not load rest of config
-  return
-end
 
 
 -- automatically source and re-compile packer whenever you save this init.lua
@@ -217,6 +294,8 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 
 
 -- CONFIG
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
 vim.cmd [[colorscheme codedark]]
 vim.o.termguicolors = true
 vim.o.updatetime = 250
@@ -237,7 +316,6 @@ vim.o.completeopt = 'menuone,noselect'
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
@@ -260,70 +338,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 
--- MASON
-require('mason').setup()
-
--- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua' }
-
--- Ensure the servers above are installed
-require('mason-lspconfig').setup {
-  ensure_installed = servers,
-}
-
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
-
-
--- LSP
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('gr', require('telescope.builtin').lsp_references)
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    if vim.lsp.buf.format then
-      vim.lsp.buf.format()
-    elseif vim.lsp.buf.formatting then
-      vim.lsp.buf.formatting()
-    end
-  end, { desc = 'Format current buffer with LSP' })
-end
-
-
--- Diagnostic keymaps
+-- DIAGNOSTIC KEYMAPS
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
