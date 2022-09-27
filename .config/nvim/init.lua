bfc69dd5a62bd8e1b394e32a0dfaf076295408b4
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2022-09-27 16:45:42 +0530

    Fix up lua some more. Mason installs everything.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,31 @@
 -- PACKER
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
+local install_path = vim.fn.stdpath 'data'
+    .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  PACKER_BOOTSTRAP = vim.fn.system({
+    'git', 'clone', '--depth', '1',
+    'https://github.com/wbthomason/packer.nvim', install_path})
+  print('Installing packer. Close and re-open neovim to begin...')
   vim.cmd [[packadd packer.nvim]]
 end
 
 
--- LSP Servers to install and setup
-LSPServers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'sumneko_lua' }
+-- reload nvim automatically
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = packer_group,
+  pattern = vim.fn.expand '$MYVIMRC',
+})
+
+
+
+-- use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	print("Packer is NOT OK")
+	return
+end
 
 
 require('packer').startup(function(use)
@@ -75,12 +91,20 @@ require('packer').startup(function(use)
     end
   }
 
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  }
+
 
   -- SESSIONS
   use {
     'rmagatti/auto-session',
     config = function()
-      require('auto-session').setup{
+      require('auto-session').setup {
         auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
         auto_session_enabled = true,
         auto_save_enabled = true,
@@ -94,7 +118,7 @@ require('packer').startup(function(use)
     'rmagatti/session-lens',
     requires = { 'rmagatti/auto-session', 'nvim-telescope/telescope.nvim' },
     config = function()
-      require('session-lens').setup{}
+      require('session-lens').setup {}
     end
   }
 
@@ -104,14 +128,18 @@ require('packer').startup(function(use)
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
-      require'nvim-tree'.setup()
-      vim.keymap.set('n', '<C-n>', require'nvim-tree.api'.tree.toggle, { desc = "Toggle Nvim Tree" })
+      require 'nvim-tree'.setup()
+      vim.keymap.set('n', '<C-n>', require 'nvim-tree.api'.tree.toggle,
+        { desc = "Toggle Nvim Tree" })
     end
   }
 
 
   -- COQ
-  use { 'ms-jpq/coq_nvim', branch = 'coq', requires = {'ms-jpq/coq.artifacts', branch = 'artifacts' } }
+  use {
+    'ms-jpq/coq_nvim', branch = 'coq',
+    requires = { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
+  }
 
 
   -- GIT
@@ -131,7 +159,7 @@ require('packer').startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     requires = { 'nvim-treesitter/nvim-treesitter-textobjects' },
     config = function()
-      require'nvim-treesitter.configs'.setup {
+      require 'nvim-treesitter.configs'.setup {
         ensure_installed = {
           'c', 'cpp', 'css', 'dockerfile', 'elm', 'fish', 'go', 'gomod',
           'haskell', 'javascript', 'json', 'lua', 'make', 'python',
@@ -144,10 +172,13 @@ require('packer').startup(function(use)
 
 
   -- LSP
-  use {
-    'neovim/nvim-lspconfig',
-    config = function()
+  use { 'williamboman/mason.nvim',
+    config = function() require 'mason'.setup {} end
+  }
 
+ use {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
       -- on_attach runs when an LSP attaches to a buffer
       local on_attach = function(_, bufnr)
         local nmap = function(keys, func, desc)
@@ -173,9 +204,12 @@ require('packer').startup(function(use)
 
         -- Lesser used LSP functionality
         nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+        nmap('<leader>D', vim.lsp.buf.type_definition,
+          'Type [D]efinition')
+        nmap('<leader>wa', vim.lsp.buf.add_workspace_folder,
+          '[W]orkspace [A]dd Folder')
+        nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder,
+          '[W]orkspace [R]emove Folder')
         nmap('<leader>wl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, '[W]orkspace [L]ist Folders')
@@ -191,28 +225,54 @@ require('packer').startup(function(use)
       end
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local lspconfig = require 'lspconfig'
+      local mason_lspconfig = require 'mason-lspconfig'
 
-      -- init lsp servers
-      local lspconfig = require('lspconfig')
-      local cfg = { on_attach=on_attach, capabilities=capabilities }
-      for _, lsp in ipairs(LSPServers) do
-        if lsp == "sumneko_lua" then
-          cfg.settings = {
-            Lua = { diagnostics = { globals = { 'vim' } } }
+      -- LSP Servers to install and setup
+      mason_lspconfig.setup {
+        ensure_installed = {
+          'bashls', 'clangd', 'cmake', 'dockerls', 'elmls',
+          'gopls', 'hls', 'jsonls', 'marksman', 'pyright',
+          'rust_analyzer', 'sqls', 'yamlls', 'zls'
+        },
+      }
+
+      mason_lspconfig.setup_handlers {
+        function (server_name)
+          lspconfig[server_name].setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+          }
+        end,
+        ['sumneko_lua'] = function()
+          lspconfig.sumneko_lua.setup {
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { 'vim' }
+                }
+              }
+            }
           }
         end
-        lspconfig[lsp].setup(cfg)
-      end
+      }
     end
   }
 
-  use { 'williamboman/mason.nvim', config = function() require'mason'.setup{} end }
+  use 'neovim/nvim-lspconfig'
 
   use {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require'mason-lspconfig'.setup {
-        ensure_installed = LSPServers
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    config = function() 
+      require 'mason-tool-installer'.setup {
+        ensure_installed = {
+          'black', 'clang-format', 'cpplint', 'elm-format',
+          'eslint-lsp', 'flake8', 'goimports', 'golangci-lint',
+          'html-lsp', 'jq', 'lua-language-server', 'markdownlint',
+          'mypy', 'prettier', 'pylint', 'shellcheck', 'shfmt',
+          'sql-formatter', 'staticcheck', 'stylua',
+          'yamlfmt', 'yamllint'
+        }
       }
     end
   }
@@ -241,22 +301,34 @@ require('packer').startup(function(use)
 
       -- see `:help telescope.builtin`
       local telescope_builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles, { desc = '[?] Find recently opened files' })
-      vim.keymap.set('n', '<leader><space>', telescope_builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to telescope to change theme, layout, etc.
-        telescope_builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer]' })
+      vim.keymap.set('n', '<leader>?', telescope_builtin.oldfiles,
+        { desc = '[?] Find recently opened files' })
+      vim.keymap.set('n', '<leader><space>', telescope_builtin.buffers,
+        { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>/',
+        function()
+          -- You can pass additional configuration to telescope to
+          -- change theme, layout, etc.
+          telescope_builtin.current_buffer_fuzzy_find(
+            require('telescope.themes').get_dropdown {
+              winblend = 10,
+              previewer = false,
+            })
+        end, { desc = '[/] Fuzzily search in current buffer]' })
 
-      vim.keymap.set('n', '<leader>ss', require('session-lens').search_session, { desc = '[S]earch [S]essions' })
-      vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, {desc = '[S]earch [B]uffers'})
+      vim.keymap.set('n', '<leader>ss',
+        require('session-lens').search_session,
+        { desc = '[S]earch [S]essions' })
+      vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files,
+        { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags,
+        { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string,
+        { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep,
+        { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers,
+        { desc = '[S]earch [B]uffers' })
     end
   }
 
@@ -272,26 +344,42 @@ require('packer').startup(function(use)
   use 'ellisonleao/glow.nvim'
   use 'tpope/vim-sleuth'
   use 'kylechui/nvim-surround'
-  use 'mhartington/formatter.nvim'
+  use {
+    'mhartington/formatter.nvim',
+    config = function()
+      -- format on save
+      vim.api.nvim_create_autocmd('BufWritePost', {
+        command = 'FormatWrite',
+        pattern = '*',
+      })
+
+      require('formatter').setup {
+        --[[ TODO: re-enable this once MacOS sed issue is fixed
+        filetype = {
+          ['*'] = {
+            require('formatter.filetypes.any').remove_trailing_whitespace
+          }
+        }
+        ]]--
+      }
+    end
+  }
 
 
   -- POETRY ENVS
   use 'petobens/poet-v'
 
 
-  if is_bootstrap then
+  if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
 end)
 
 
--- automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
-  group = packer_group,
-  pattern = vim.fn.expand '$MYVIMRC',
-})
+-- stop loading config if bootstrapping
+if PACKER_BOOTSTRAP then
+  return
+end
 
 
 -- CONFIG
@@ -314,7 +402,8 @@ vim.o.completeopt = 'menuone,noselect'
 -- KEYMAPS
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+--  NOTE: Must happen before plugins are required
+--  (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -323,13 +412,16 @@ vim.g.maplocalleader = ' '
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'",
+  { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'",
+  { expr = true, silent = true })
 
 
 -- YANK HIGHLIGHT
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight',
+  { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
