e23217b8e5eb2a98ce94718087c978729af45a7c
Ananth Bhaskararaman <antsub@gmail.com>
2023-03-26 21:21:43 +0530

    Fix formatting

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -80,7 +80,7 @@ return {
 		},
 		config = function()
 			local capabilities =
-			    require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 			local lsp_handlers = {
 				function(server_name)
 					require("lspconfig")[server_name].setup({
