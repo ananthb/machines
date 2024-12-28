94f2c35245f824da8a51f3642ac372a775d2d7a7
Ananth Bhaskararaman <antsub@gmail.com>
2024-12-28 22:23:04 +0530

    Switch default locale

--- flake.nix
+++ flake.nix
@@ -191,7 +191,7 @@
                 time.timeZone = "Asia/Kolkata";
 
                 # Select internationalisation properties.
-                # i18n.defaultLocale = "en_US.UTF-8";
+                i18n.defaultLocale = "en_IN.UTF-8";
                 # console = {
                 #   font = "Lat2-Terminus16";
                 #   keyMap = "us";
