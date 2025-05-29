ee40f9fcfb64f37e319af7dbb057ab5e426477fc
Ananth Bhaskararaman <antsub@gmail.com>
2025-05-29 22:13:28 +0530

    remove arrs

--- hosts/enterprise/default.nix
+++ hosts/enterprise/default.nix
@@ -1,16 +1,27 @@
 { ... }:
 
 {
-  homebrew.brews = [ "nut" "dump1090-mutability" "readsb" ];
+  homebrew.brews = [
+    "nut"
+    "readsb"
+  ];
   homebrew.casks =
-    [ "wireshark" "jellyfin" "kopiaui" "qbittorrent" "cloudflare-warp" ];
+    [ "wireshark" "jellyfin" "kopiaui" "utm" ];
   homebrew.masApps = { };
 
   launchd = {
     user = {
       agents = {
-        dump1090 = {
-          command = "dump1090 --net --aggressive";
+        readsb = {
+          command = "/opt/homebrew/bin/readsb --net --aggressive";
+          serviceConfig = {
+            KeepAlive = true;
+            RunAtLoad = true;
+          };
+        };
+
+        tsnet-serve-tv = {
+          command = "/Users/ananth/Downloads/tsnet-serve -funnel -hostname tv -state-dir /Users/ananth/Downloads/state -backend localhost:8096";
           serviceConfig = {
             KeepAlive = true;
             RunAtLoad = true;
