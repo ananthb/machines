c64365114442eccbe294447b88d92356f379510f
Ananth Bhaskararaman <antsub@gmail.com>
2025-05-31 21:38:43 +0530

    readsb options

--- hosts/enterprise/default.nix
+++ hosts/enterprise/default.nix
@@ -13,7 +13,7 @@
     user = {
       agents = {
         readsb = {
-          command = "/opt/homebrew/bin/readsb --device-type rtlsdr --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004 --uuid-file ~/Library/Application\ Support/readsb/airplanes.live/airplanes-uuid";
+          command = "/opt/homebrew/bin/readsb --gain auto --device-type rtlsdr --ppm 0 --max-range 350 --modeac --net --net-heartbeat 60 --net-heartbeat 60 --net-ro-size 1200 --net-ro-interval 0.1 --net-ri-port 30001 --net-ro-port 30002 --net-sbs-port 30003 --net-bo-port 30005 --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004 --uuid-file ~/Library/Application\ Support/readsb/airplanes.live/airplanes-uuid";
           serviceConfig = {
             KeepAlive = true;
             RunAtLoad = true;
