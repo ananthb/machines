84b8ffc2e2e0cb348744b5d1e0817cca9eb92448
Ananth Bhaskararaman <antsub@gmail.com>
2025-06-07 17:57:24 +0530

    add meilisearch

--- hosts/enterprise/default.nix
+++ hosts/enterprise/default.nix
@@ -3,6 +3,7 @@
 {
   homebrew.brews = [
     "readsb"
+    "meilisearch"
   ];
   homebrew.casks = [
     "wireshark"
@@ -13,18 +14,16 @@
   homebrew.masApps = { };
 
   launchd = {
-    daemons = {
-      readsb = {
-        command = "/opt/homebrew/bin/readsb --quiet --gain auto --lat 13.0291 --lon 80.2585 --device-type rtlsdr --ppm 0 --max-range 350 --modeac --net --net-heartbeat 60 --net-heartbeat 60 --net-ro-size 1200 --net-ro-interval 0.1 --net-ri-port 30001 --net-ro-port 30002 --net-sbs-port 30003 --net-bo-port 30005 --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004 --uuid-file ~/Library/Application Support/readsb/airplanes.live/airplanes-uuid";
-        serviceConfig = {
-          KeepAlive = true;
-          RunAtLoad = true;
-        };
-      };
-    };
-
     user = {
       agents = {
+        readsb = {
+          command = "/opt/homebrew/bin/readsb --quiet --gain auto --device-type rtlsdr --ppm 0 --max-range 350 --modeac --net --net-heartbeat 60 --net-heartbeat 60 --net-ro-size 1200 --net-ro-interval 0.1 --net-ri-port 30001 --net-ro-port 30002 --net-sbs-port 30003 --net-bo-port 30005 --net-connector feed.airplanes.live,30004,beast_reduce_plus_out,feed.airplanes.live,64004 --uuid-file ~/Library/Application Support/readsb/airplanes.live/airplanes-uuid";
+          serviceConfig = {
+            KeepAlive = true;
+            RunAtLoad = true;
+          };
+        };
+
         tsnet-serve-tv = {
           command = "/Users/ananth/Downloads/tsnet-serve -funnel -hostname tv -state-dir ~/Library/Application Support/tsnet-serve/tv -backend localhost:8096";
           serviceConfig = {
