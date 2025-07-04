76dd83819eefa42bbc5ac83b712aba34c071300c
Ananth Bhaskararaman <antsub@gmail.com>
2025-07-04 12:50:36 +0530

    transmission scheduling options

--- hosts/endeavour/services.nix
+++ hosts/endeavour/services.nix
@@ -26,6 +26,9 @@
   # Enable tailscale
   tailscale.enable = true;
 
+  cloudflare-warp.enable = true;
+  cloudflare-warp.openFirewall = false;
+
   grafana = {
     enable = true;
   };
@@ -69,8 +72,20 @@
     enable = true;
     package = pkgs.transmission_4;
     group = "media";
+    downloadDirPermissions = "770";
     settings = {
       umask = "002";
+      proxy_url = "socks5://localhost:8080";
+      
+      alt-speed-up = 1000;    # 1000KB/s
+      alt-speed-down = 1000;  # 1000KB/s
+
+      # Scheduling option docs:
+      # https://github.com/transmission/transmission/blob/main/docs/Editing-Configuration-Files.md#scheduling
+      alt-speed-time-enabled = true;
+      alt-speed-time-begin = 540;     # 9am
+      alt-speed-time-end = 1020;      # 5pm
+      alt-speed-time-day = 127;       # all days of the week
     };
   };
 
