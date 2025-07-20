4b35ca0e9854c1993221a05e5818bedc1d7de559
Ananth Bhaskararaman <antsub@gmail.com>
2025-07-20 18:57:17 +0530

    rm samba

--- hosts/endeavour/services/default.nix
+++ hosts/endeavour/services/default.nix
@@ -31,41 +31,6 @@
     cloudflare-warp.enable = true;
     cloudflare-warp.openFirewall = false;
 
-    samba = {
-      enable = true;
-      securityType = "user";
-      openFirewall = true;
-      settings = {
-        global = {
-          "workgroup" = "WORKGROUP";
-          "server string" = "Samba Server %v";
-          "netbios name" = "${hostname}";
-          "security" = "user";
-          "use sendfile" = "yes";
-          # note: localhost is the ipv6 localhost ::1
-          "hosts allow" = "10.15.16.0 127.0.0.1 localhost";
-          "hosts deny" = "0.0.0.0/0";
-          "guest account" = "nobody";
-          "map to guest" = "bad user";
-        };
-        "media" = {
-          "path" = "/var/lib/media";
-          "browseable" = "yes";
-          "read only" = "no";
-          "guest ok" = "yes";
-          "create mask" = "0644";
-          "directory mask" = "0755";
-          "force user" = "jellyfin";
-          "force group" = "media";
-        };
-      };
-    };
-
-    samba-wsdd = {
-      enable = true;
-      openFirewall = true;
-    };
-
     tsnsrv = {
       enable = true;
       defaults.authKeyPath = config.sops.secrets."tsnsrv/auth_key".path;
