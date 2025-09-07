36f9f7ec4552225d0a728921f2de18b035b3bd09
Ananth Bhaskararaman <antsub@gmail.com>
2025-09-07 11:16:30 +0530

    fix tailscale with kernel patch

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   nixpkgs-unstable,
+  username,
   ...
 }:
 {
@@ -19,6 +20,12 @@
 
   environment.systemPackages = with pkgs; [
     git-credential-manager
+  ];
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINu7u4V6khhhUvepvptel86DN3XMCwZVdQe/7P6WW1KmAAAAFXNzaDphbmFudGhzLXNzaC1rZXktMQ== ananth@yubikey-5c"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFCVZPWg3DVxjuORNKJnjaRSPoZ4nYnzM070q0fIeM32AAAAG3NzaDphbmFudGhzLXNzaC1rZXktNWMtbmFubw== ananth@yubikey-5c-nano
+"
   ];
 
   sops.defaultSopsFile = ../secrets.yaml;
--- hosts/endeavour/hardware-configuration.nix
+++ hosts/endeavour/hardware-configuration.nix
@@ -5,6 +5,7 @@
   config,
   lib,
   modulesPath,
+  pkgs,
   ...
 }:
 
@@ -27,6 +28,18 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ "bcachefs" ];
+  boot.kernelPatches = [
+    # Fix the /proc/net/tcp seek issue
+    # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
+    {
+      name = "proc: fix missing pde_set_flags() for net proc files";
+      patch = pkgs.fetchurl {
+        name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
+        url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
+        hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
+      };
+    }
+  ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/ba19dc35-0edc-4769-a69a-a947410a8a60";
