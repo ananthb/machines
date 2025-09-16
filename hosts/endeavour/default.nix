daec25a76512ce9920c4aa83c3142bdfe46ab59e
Ananth Bhaskararaman <antsub@gmail.com>
2025-09-16 19:14:57 +0530

    add jellyseerr

--- hosts/endeavour/default.nix
+++ hosts/endeavour/default.nix
@@ -14,6 +14,7 @@
     ./open-webui.nix
     ./power.nix
     ./seafile.nix
+    ./streaming.nix
   ];
 
   sops.secrets."gcloud/service_accounts/kopia-hathi-backups.json" = { };
@@ -106,46 +107,6 @@
   sops.secrets."passwords/nut/nutmon".mode = "0444";
 
   # Jellyfin
-  services = {
-    jellyfin.enable = true;
-    jellyfin.group = "media";
-    jellyfin.openFirewall = true;
-
-    meilisearch.enable = true;
-    meilisearch.package = pkgs.meilisearch;
-    meilisearch.listenAddress = "[::]";
-
-    tsnsrv.services.tv = {
-      funnel = true;
-      urlParts.port = 8096;
-    };
-  };
-
-  systemd.services.tsnsrv-tv.wants = [ "jellyfin.service" ];
-  systemd.services.tsnsrv-tv.after = [ "jellyfin.service" ];
-
-  nixpkgs.overlays = [
-    # Modify jellyfin-web index.html for the intro-skipper plugin to work.
-    # intro skipper plugin has to be installed from the UI.
-    (final: prev: {
-      jellyfin-web = prev.jellyfin-web.overrideAttrs (
-        finalAttrs: previousAttrs: {
-          installPhase = ''
-            runHook preInstall
-
-            # this is the important line
-            sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-
-            mkdir -p $out/share
-            cp -a dist $out/share/jellyfin-web
-
-            runHook postInstall
-          '';
-        }
-      );
-    })
-  ];
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
---
+++ hosts/endeavour/streaming.nix
@@ -0,0 +1,78 @@
+{
+  pkgs,
+  ...
+}:
+{
+  services = {
+    jellyfin.enable = true;
+    jellyfin.group = "media";
+    jellyfin.openFirewall = true;
+
+    meilisearch.enable = true;
+    meilisearch.package = pkgs.meilisearch;
+    meilisearch.listenAddress = "[::]";
+
+    tsnsrv.services.tv = {
+      funnel = true;
+      urlParts.port = 8096;
+    };
+
+    jellyseerr.enable = true;
+
+    tsnsrv.services.watch = {
+      funnel = true;
+      urlParts.port = 5055;
+    };
+
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ "jellyseerr" ];
+      ensureUsers = [
+        {
+          name = "jellyseerr";
+          ensureDBOwnership = true;
+          ensureClauses.login = true;
+        }
+      ];
+    };
+
+  };
+
+  systemd.services = {
+    tsnsrv-watch.wants = [ "jellyseerr.service" ];
+    tsnsrv-watch.after = [ "jellyseerr.service" ];
+
+    jellyseerr.environment = {
+      DB_TYPE = "postgres";
+      DB_SOCKET_PATH = "/var/run/postgresql";
+      DB_USER = "jellyseerr";
+      DB_NAME = "jellyseerr";
+    };
+  };
+
+  systemd.services.tsnsrv-tv.wants = [ "jellyfin.service" ];
+  systemd.services.tsnsrv-tv.after = [ "jellyfin.service" ];
+
+  nixpkgs.overlays = [
+    # Modify jellyfin-web index.html for the intro-skipper plugin to work.
+    # intro skipper plugin has to be installed from the UI.
+    (final: prev: {
+      jellyfin-web = prev.jellyfin-web.overrideAttrs (
+        finalAttrs: previousAttrs: {
+          installPhase = ''
+            runHook preInstall
+
+            # this is the important line
+            sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
+
+            mkdir -p $out/share
+            cp -a dist $out/share/jellyfin-web
+
+            runHook postInstall
+          '';
+        }
+      );
+    })
+  ];
+
+}
