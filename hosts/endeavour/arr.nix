9993d2c314f76b75e542de39008e2ba06e38550c
Ananth Bhaskararaman <antsub@gmail.com>
2025-09-20 02:35:03 +0530

    docs

--- hosts/endeavour/arr.nix
+++ hosts/endeavour/arr.nix
@@ -4,6 +4,9 @@
 }:
 
 {
+  # WARP must be manually set up in proxy mode listening on port 8888.
+  # This involves registering a new identity, accepting the tos,
+  # setting the mode to proxy, and then setting proxy port to 8888.
   services.cloudflare-warp.enable = true;
   services.cloudflare-warp.openFirewall = false;
 
