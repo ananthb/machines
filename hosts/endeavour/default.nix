07d6203b403badac91f8b86ab8742832555ecd66
Ananth Bhaskararaman <antsub@gmail.com>
2025-09-20 02:55:59 +0530

    move open-webui to voyager as well

--- hosts/endeavour/default.nix
+++ hosts/endeavour/default.nix
@@ -12,7 +12,6 @@
     ./hardware-configuration.nix
     ./immich.nix
     ./jellyfin.nix
-    ./open-webui.nix
     ./power.nix
     ./seafile.nix
   ];
--- hosts/voyager/open-webui.nix
+++ hosts/voyager/open-webui.nix
@@ -5,9 +5,13 @@
 }:
 
 {
-  #
+  # WARP must be manually set up in proxy mode listening on port 8888.
+  # This involves registering a new identity, accepting the tos,
+  # setting the mode to proxy, and then setting proxy port to 8888.
+  services.cloudflare-warp.enable = true;
+  services.cloudflare-warp.openFirewall = false;
+
   # Open WebUI
-  #
   services.open-webui = {
     enable = true;
     package = pkgs.open-webui.overrideAttrs (old: {
