5ad849879cba7d30cf965c5ceaa538f85d7cb805
Ananth Bhaskararaman <antsub@gmail.com>
2025-10-14 17:22:25 +0530

    remove extra mount

--- hosts/endeavour/hardware-configuration.nix
+++ hosts/endeavour/hardware-configuration.nix
@@ -49,11 +49,6 @@
     fsType = "bcachefs";
   };
 
-  fileSystems."/export/media" = {
-    device = "/srv/media";
-    options = [ "bind" ];
-  };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
