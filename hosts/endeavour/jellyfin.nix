52a708f15e199057bfeb24d8396fb613be991b3b
Ananth Bhaskararaman <antsub@gmail.com>
2025-11-11 20:14:07 +0530

    remove intro skipper fragment

--- hosts/endeavour/jellyfin.nix
+++ hosts/endeavour/jellyfin.nix
@@ -29,26 +29,4 @@
   systemd.services.tsnsrv-tv.wants = [ "jellyfin.service" ];
   systemd.services.tsnsrv-tv.after = [ "jellyfin.service" ];
 
-  nixpkgs.overlays = [
-    # Modify jellyfin-web index.html for the intro-skipper plugin to work.
-    # intro skipper plugin has to be installed from the UI.
-    (final: prev: {
-      jellyfin-web = prev.jellyfin-web.overrideAttrs (
-        finalAttrs: previousAttrs: {
-          installPhase = ''
-            runHook preInstall
-
-            # this is the important line
-            sed -i "s#</head>#<script src=\"configurationpage?name=skip-intro-button.js\"></script></head>#" dist/index.html
-
-            mkdir -p $out/share
-            cp -a dist $out/share/jellyfin-web
-
-            runHook postInstall
-          '';
-        }
-      );
-    })
-  ];
-
 }
