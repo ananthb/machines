30e19c74efaf4ee86280d1d59c2ca91576e5bfde
Ananth Bhaskararaman <antsub@gmail.com>
2025-11-14 02:29:17 +0530

    open more stuff why not

--- hosts/endeavour/cftunnel.nix
+++ hosts/endeavour/cftunnel.nix
@@ -5,14 +5,14 @@
     tunnels."kedi" = {
       default = "http_status:404";
       ingress = {
-        "6a.kedi.dev" = "http://voyager:8123";
-        "actual.kedi.dev" = "http://voyager:3100";
-        "immich.kedi.dev" = "http://endeavour:2283";
-        "mealie.kedi.dev" = "http://voyager:9000";
-        "open-webui.kedi.dev" = "http://endeavour:8090";
-        "radicale.kedi.dev" = "http://voyager:5232";
-        "seafile.kedi.dev" = "http://endeavour:4000";
-        "vault.kedi.dev" = "http://voyager:8222";
+        "6a.kedi.dev" = "http://voyager.local:8123";
+        "actual.kedi.dev" = "http://voyager.local:3100";
+        "immich.kedi.dev" = "http://localhost:2283";
+        "mealie.kedi.dev" = "http://voyager.local:9000";
+        "open-webui.kedi.dev" = "http://localhost:8090";
+        "radicale.kedi.dev" = "http://voyager.local:5232";
+        "seafile.kedi.dev" = "http://localhost:4000";
+        "vault.kedi.dev" = "http://voyager.local:8222";
       };
       credentialsFile =
         config.sops.secrets."cloudflared/tunnels/5fd5fbd5-fc21-4766-b92e-a8b577b4bda5/credentials".path;
