0003fbc8330e0fe5bae2cfea74ef42bfd9a6bcb6
Ananth Bhaskararaman <antsub@gmail.com>
2025-11-15 22:36:35 +0530

    more ipv4 proxy stuff

--- hosts/endeavour/seafile.nix
+++ hosts/endeavour/seafile.nix
@@ -200,7 +200,7 @@
     enable = true;
     globalConfig = ''
       servers {
-        trusted_proxies static ::1 127.0.0.1
+        trusted_proxies static ::1 127.0.0.1 10.15.16.202
       }
     '';
     virtualHosts.":4000" = {
