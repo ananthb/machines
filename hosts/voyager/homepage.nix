60500595832cc82ffed3e86112924052d91d2a0d
Ananth Bhaskararaman <antsub@gmail.com>
2025-11-15 18:15:54 +0530

    icons

--- hosts/voyager/homepage.nix
+++ hosts/voyager/homepage.nix
@@ -1,32 +1,40 @@
-{ }:
+{ ... }:
 {
   services.homepage-dashboard = {
     enable = true;
     openFirewall = true;
     allowedHosts = "apps.kedi.dev";
+    settings = {
+      base = "https://apps.kedi.dev";
+      target = "_blank";
+    };
     services = [
       {
         "Productivity" = [
           {
             "Seafile" = {
+              icon = "seafile";
               description = "Files & Collaboration";
               href = "https://seafile.kedi.dev";
             };
           }
           {
             "Immich" = {
+              icon = "immich";
               description = "Photo & Video Library";
               href = "https://immich.kedi.dev";
             };
           }
           {
             "Mealie" = {
+              icon = "mealie";
               description = "Recipes & Meal Planner";
               href = "https://mealie.kedi.dev";
             };
           }
           {
             "Open WebUI" = {
+              icon = "open-webui";
               description = "Large Language Model (LLM) inference";
               href = "https://open-webui.kedi.dev";
             };
@@ -37,18 +45,21 @@
         "Organisation" = [
           {
             "Radicale" = {
+              icon = "radicale";
               description = "Contacts, Calendars, & Tasks";
               href = "https://radicale.kedi.dev";
             };
           }
           {
             "Actual" = {
+              icon = "actual";
               description = "Personal Budget Tracker";
               href = "https://actual.kedi.dev";
             };
           }
           {
             "Vaultwarden" = {
+              icon = "vaultwarden";
               description = "Password Manager";
               href = "https://vault.kedi.dev";
             };
@@ -59,12 +70,14 @@
         "Arr" = [
           {
             "Jellyseerr" = {
+              icon = "jellyseerr";
               description = "Watch requests";
               href = "http://voyager:5055";
             };
           }
           {
             "Radarr" = {
+              icon = "radarr";
               description = "Movies";
               href = "http://endeavour:7878";
             };
@@ -72,18 +85,21 @@
           }
           {
             "Sonarr" = {
+              icon = "sonarr";
               description = "TV Shows";
               href = "http://endeavour:8989";
             };
           }
           {
             "Prowlarr" = {
+              icon = "prowlarr";
               description = "Indexer";
               href = "http://endeavour:9696";
             };
           }
           {
             "qBittorrent" = {
+              icon = "qbittorrent";
               description = "Torrent downloader";
               href = "http://endeavour:8080";
             };
@@ -94,18 +110,21 @@
         "Internal" = [
           {
             "Grafana" = {
+              icon = "grafana";
               description = "Monitoring";
               href = "https://mon.tail42937.ts.net";
             };
           }
           {
             "VictoriaMetrics" = {
+              icon = "victoriametrics";
               description = "Metrics";
               href = "http://voyager:8428";
             };
           }
           {
             "Upptime" = {
+              icon = "mdi-arrow-up-circle";
               description = "Uptime Status Website";
               href = "uptime.kedi.dev";
             };
