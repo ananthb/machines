dd782ed818526eaa353db57cde16273ef7b9f5d4
Ananth Bhaskararaman <antsub@gmail.com>
2025-12-05 02:07:32 +0530

    proxy

--- hosts/voyager/default.nix
+++ hosts/voyager/default.nix
@@ -38,6 +38,12 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
+  # WARP must be manually set up in proxy mode listening on port 8888.
+  # This involves registering a new identity, accepting the tos,
+  # setting the mode to proxy, and then setting proxy port to 8888.
+  services.cloudflare-warp.enable = true;
+  services.cloudflare-warp.openFirewall = false;
+
   power.ups = {
     enable = true;
     mode = "netclient";
