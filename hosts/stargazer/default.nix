0002cb10e2b40247eede398f736487f2ab2d575e
Ananth Bhaskararaman <antsub@gmail.com>
2025-12-16 01:43:15 +0530

    stargazer doesn't need warp

--- hosts/stargazer/default.nix
+++ hosts/stargazer/default.nix
@@ -28,12 +28,6 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # WARP must be manually set up in proxy mode listening on port 8888.
-  # This involves registering a new identity, accepting the tos,
-  # setting the mode to proxy, and then setting proxy port to 8888.
-  services.cloudflare-warp.enable = true;
-  services.cloudflare-warp.openFirewall = false;
-
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
