1e22319d034eda8c3b992e295940ee608e59d2bd
Ananth Bhaskararaman <antsub@gmail.com>
2025-12-17 01:24:11 +0530

    stuff

--- hosts/voyager/cftunnel.nix
+++ hosts/voyager/cftunnel.nix
@@ -13,6 +13,8 @@
         "open-webui.kedi.dev" = "http://[fdc0:6625:5195::50]:8090";
         "seafile.kedi.dev" = "http://[fdc0:6625:5195::50]:4000";
         "seerr.kedi.dev" = "http://localhost:5055";
+        # TODO: enable when we've moved it back to voyager
+	# "vault.kedi.dev" = "http://localhost:8222";
         "wallabag.kedi.dev" = "http://localhost:8085";
       };
       credentialsFile =
