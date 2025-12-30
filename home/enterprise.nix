9cc55bd0c27f949d2651ec97e9f840d93d858b78
Ananth Bhaskararaman <antsub@gmail.com>
2025-12-31 01:06:59 +0530

    nosleep

--- home/enterprise.nix
+++ home/enterprise.nix
@@ -37,6 +37,10 @@
   dconf = {
     enable = true;
     settings = {
+      "org/gnome/settings-daemon/plugins/power" = {
+        sleep-inactive-ac-type = "nothing";
+	sleep-inactive-ac-timeout = 0;
+      };
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = with pkgs.gnomeExtensions; [
