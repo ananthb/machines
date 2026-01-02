e508d96c9327841206376136ea9d5dfaaa192cb7
Ananth Bhaskararaman <antsub@gmail.com>
2026-01-02 21:46:46 +0530

    caddy trusted ips

--- services/caddy.nix
+++ services/caddy.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ trustedIPs, ... }:
 {
   services.caddy = {
     enable = true;
     globalConfig = ''
       servers {
-        trusted_proxies static ::1 127.0.0.0/8 fdc0:6625:5195::0/64 10.15.16.0/24
+        trusted_proxies static ${trustedIPs}
       }
     '';
   };
