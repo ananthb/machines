e66c9fe8fd4c5e0ea2b75483b6009dbaeb26b88d
Ananth Bhaskararaman <antsub@gmail.com>
2026-01-10 18:50:40 +0530

    start maybe

--- services/monitoring/libvirt.nix
+++ services/monitoring/libvirt.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   services.prometheus.exporters.libvirt = {
     enable = true;
     openFirewall = true;
+  };
+
+  systemd.services.prometheus-libvirt-exporter.serviceConfig = {
+    ExecStart = lib.mkForce ''
+      ${pkgs.prometheus-libvirt-exporter}/bin/libvirt-exporter \
+        --web.listen-address [::]:9177
+    '';
+    RestrictAddressFamilies = lib.mkForce [ "AF_INET" "AF_INET6" "AF_UNIX" ];
+    SupplementaryGroups = [ "libvirtd" ];
   };
 }
