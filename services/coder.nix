0e31498a3050f2a0afcdcd7248cd852b1ac58f71
Ananth Bhaskararaman <antsub@gmail.com>
2026-01-14 23:32:17 +0530

    coder

---
+++ services/coder.nix
@@ -0,0 +1,27 @@
+{ config, ... }:
+{
+  services.coder = {
+    enable = true;
+    accessUrl = "https://coder.kedi.dev";
+    listenAddress = "[::1]:3030";
+    environment = {
+      extra = {
+        CODER_OAUTH2_GITHUB_ALLOW_SIGNUPS = "true";
+        CODER_OAUTH2_GITHUB_ALLOWED_ORGS = "ananthb";
+      };
+      file = config.sops.templates."coder/env".path;
+    };
+  };
+
+  sops.templates."coder/env" = {
+    text = ''
+      CODER_OAUTH2_GITHUB_CLIENT_ID=${config.sops.placeholder."github/oauth/kedi-coder/id"}
+      CODER_OAUTH2_GITHUB_CLIENT_SECRET=${config.sops.placeholder."github/oauth/kedi-coder/secret"}
+    '';
+  };
+
+  sops.secrets = {
+    "github/oauth/kedi-coder/id" = { };
+    "github/oauth/kedi-coder/secret" = { };
+  };
+}
