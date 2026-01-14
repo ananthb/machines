e771576ea1e27bef43f3e24f0ee534c7841bbf8b
Ananth Bhaskararaman <antsub@gmail.com>
2026-01-15 01:52:40 +0530

    sf

--- hosts/endeavour/rclone.nix
+++ hosts/endeavour/rclone.nix
@@ -1,31 +1,19 @@
 { config, ... }:
 {
-  # Expected secrets.yaml structure for rclone/conf:
-  #
-  # [webdav]
-  # type = webdav
-  # user = <user>
-  # pass = <rclone obscure password>
-  #
-  # [gdrive]
-  # type = drive
-  # scope = drive
-  # token = <json from `rclone authorize "drive"`>
-
-  sops.secrets."rclone/conf" = { };
-
   my-services.rclone-syncs."taxes-sync" = {
     type = "bisync";
-    source = "webdav:";
+    source = "seafile:";
     sourceSubPath = "/Finance/Taxes";
     destination = "gdrive:";
     destSubPath = "/Taxes";
     rcloneConfig = config.sops.secrets."rclone/conf".path;
     interval = "daily";
     environment = {
-      RCLONE_CONFIG_WEBDAV_URL = "http://localhost:4000/seafdav";
-      RCLONE_CONFIG_WEBDAV_TYPE = "webdav";
-      RCLONE_CONFIG_WEBDAV_VENDOR = "other";
+      RCLONE_CONFIG_SEAFILE_URL = "http://localhost:4444/seafdav";
+      RCLONE_CONFIG_SEAFILE_TYPE = "webdav";
+      RCLONE_CONFIG_SEAFILE_VENDOR = "other";
     };
   };
+
+  sops.secrets."rclone/conf" = { };
 }
