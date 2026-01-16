67bcaff3208d17e5c7ad97296aeb736681b8174b
Ananth Bhaskararaman <antsub@gmail.com>
2026-01-16 15:17:27 +0530

    new sync jobs

--- hosts/endeavour/rclone.nix
+++ hosts/endeavour/rclone.nix
@@ -1,20 +1,57 @@
 { config, ... }:
 {
-  my-services.rclone-syncs."taxes-sync" = {
-    type = "bisync";
-    source = "seafile:";
-    sourceSubPath = "/Finance/Taxes";
-    destination = "gdrive:";
-    destSubPath = "/Finance/Taxes";
-    checkAccess = false;
-    rcloneConfig = config.sops.secrets."rclone/conf".path;
-    interval = "*:0/5";
-    environment = {
-      RCLONE_CONFIG_SEAFILE_URL = "http://localhost:4444/seafdav";
-      RCLONE_CONFIG_SEAFILE_TYPE = "webdav";
-      RCLONE_CONFIG_SEAFILE_VENDOR = "other";
+  my-services.rclone-syncs = {
+    "ananth-taxes-sync" = {
+      type = "bisync";
+      source = "seafile:";
+      sourceSubPath = "/Finance/Taxes";
+      destination = "gdrive:";
+      destSubPath = "/Finance/Taxes";
+      checkAccess = false;
+      rcloneConfig = config.sops.secrets."rclone/ananth".path;
+      interval = "*:0/5";
+      environment = {
+        RCLONE_CONFIG_SEAFILE_URL = "http://localhost:4444/seafdav";
+        RCLONE_CONFIG_SEAFILE_TYPE = "webdav";
+        RCLONE_CONFIG_SEAFILE_VENDOR = "other";
+      };
+    };
+
+    "bhaskar-documents-sync" = {
+      type = "bisync";
+      source = "seafile:";
+      sourceSubPath = "/Documents";
+      destination = "gdrive:";
+      destSubPath = "/Documents";
+      checkAccess = false;
+      rcloneConfig = config.sops.secrets."rclone/bhaskar".path;
+      interval = "*:0/5";
+      environment = {
+        RCLONE_CONFIG_SEAFILE_URL = "http://localhost:4444/seafdav";
+        RCLONE_CONFIG_SEAFILE_TYPE = "webdav";
+        RCLONE_CONFIG_SEAFILE_VENDOR = "other";
+      };
+    };
+
+    "bhaskar-family-library-sync" = {
+      type = "bisync";
+      source = "seafile:";
+      sourceSubPath = "/Family Library";
+      destination = "gdrive:";
+      destSubPath = "/Reference";
+      checkAccess = false;
+      rcloneConfig = config.sops.secrets."rclone/bhaskar".path;
+      interval = "*:0/5";
+      environment = {
+        RCLONE_CONFIG_SEAFILE_URL = "http://localhost:4444/seafdav";
+        RCLONE_CONFIG_SEAFILE_TYPE = "webdav";
+        RCLONE_CONFIG_SEAFILE_VENDOR = "other";
+      };
     };
   };
 
-  sops.secrets."rclone/conf" = { };
+  sops.secrets = {
+    "rclone/ananth" = { };
+    "rclone/bhaskar" = { };
+  };
 }
