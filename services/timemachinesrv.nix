b8f4cb4a169813f0c0f33930e4750a8857488ead
Ananth Bhaskararaman <antsub@gmail.com>
2026-02-02 14:47:55 +0530

    start samba after tailscale

--- services/timemachinesrv.nix
+++ services/timemachinesrv.nix
@@ -10,7 +10,7 @@ _: {
           "server string" = "Time Machine";
           "server role" = "standalone server";
 
-          # Listen only on Tailscale (lo required for internal communication)
+          # Listen only on Tailscale
           interfaces = "lo tailscale0";
           "bind interfaces only" = "yes";
 
@@ -49,10 +49,41 @@ _: {
         enable = true;
         userServices = true;
       };
+      extraServiceFiles = {
+        smb = ''
+          <?xml version="1.0" standalone='no'?>
+          <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+          <service-group>
+            <name replace-wildcards="yes">%h</name>
+            <service>
+              <type>_smb._tcp</type>
+              <port>445</port>
+            </service>
+            <service>
+              <type>_device-info._tcp</type>
+              <port>9</port>
+              <txt-record>model=TimeCapsule8,119</txt-record>
+            </service>
+            <service>
+              <type>_adisk._tcp</type>
+              <port>9</port>
+              <txt-record>dk0=adVN=timemachine,adVF=0x82</txt-record>
+              <txt-record>sys=adVF=0x100</txt-record>
+            </service>
+          </service-group>
+        '';
+      };
     };
   };
 
-  systemd.tmpfiles.rules = [
-    "d /srv/timemachine 0777 nobody nogroup -"
-  ];
+  systemd = {
+    tmpfiles.rules = [
+      "d /srv/timemachine 0777 nobody nogroup -"
+    ];
+
+    services.samba-smbd = {
+      after = [ "tailscaled.service" ];
+      wants = [ "tailscaled.service" ];
+    };
+  };
 }
