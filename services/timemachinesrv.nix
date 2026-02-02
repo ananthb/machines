e7279b307d391f686f23f70a35ae43aecb757a1d
Ananth Bhaskararaman <antsub@gmail.com>
2026-02-02 14:53:03 +0530

    avahi over tailscale

--- services/timemachinesrv.nix
+++ services/timemachinesrv.nix
@@ -4,15 +4,11 @@ _: {
     samba = {
       enable = true;
       nmbd.enable = false;
-      openFirewall = true;
+      openFirewall = false;
       settings = {
         global = {
           "server string" = "Time Machine";
           "server role" = "standalone server";
-
-          # Listen only on Tailscale
-          interfaces = "lo tailscale0";
-          "bind interfaces only" = "yes";
 
           # Guest access
           "map to guest" = "Bad User";
@@ -41,39 +37,6 @@ _: {
       };
     };
 
-    # Avahi for Time Machine discovery
-    avahi = {
-      enable = true;
-      nssmdns4 = true;
-      publish = {
-        enable = true;
-        userServices = true;
-      };
-      extraServiceFiles = {
-        smb = ''
-          <?xml version="1.0" standalone='no'?>
-          <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-          <service-group>
-            <name replace-wildcards="yes">%h</name>
-            <service>
-              <type>_smb._tcp</type>
-              <port>445</port>
-            </service>
-            <service>
-              <type>_device-info._tcp</type>
-              <port>9</port>
-              <txt-record>model=TimeCapsule8,119</txt-record>
-            </service>
-            <service>
-              <type>_adisk._tcp</type>
-              <port>9</port>
-              <txt-record>dk0=adVN=timemachine,adVF=0x82</txt-record>
-              <txt-record>sys=adVF=0x100</txt-record>
-            </service>
-          </service-group>
-        '';
-      };
-    };
   };
 
   systemd = {
