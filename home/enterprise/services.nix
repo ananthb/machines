6230073977b29fa9edc22f722099b17d6e7ac53d
Ananth Bhaskararaman <antsub@gmail.com>
2026-03-04 01:05:33 +0530

    fix enterprise

--- home/enterprise/services.nix
+++ home/enterprise/services.nix
@@ -1,6 +1,15 @@
 _: {
   services = {
     activitywatch.enable = true;
-    gnome-keyring.enable = true;
+    gnome-keyring = {
+      enable = true;
+      # Avoid gnome-keyring's SSH agent since it can't sign FIDO sk keys reliably.
+      components = [
+        "pkcs11"
+        "secrets"
+      ];
+    };
+    # Use OpenSSH's agent for FIDO/U2F sk keys.
+    ssh-agent.enable = true;
   };
 }
