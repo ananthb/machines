57743f7f137c42e97d294e6c0c960ae461535322
Ananth Bhaskararaman <antsub@gmail.com>
2026-03-26 01:24:48 +0530

    fix git

--- home/discovery.nix
+++ home/discovery.nix
@@ -14,29 +14,35 @@
     };
   };
 
-  programs.git.settings = {
-    gpg.format = "ssh";
-    user.signingkey = "~/.ssh/yubikey_5c_nano";
-    commit.gpgsign = "true";
-    credential = {
-      helper = "!gh auth git-credential";
-      "https://github.com".username = "ananthb";
-    };
-  };
-
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-    matchBlocks = {
-      "endeavour.local 10.15.16.123 stargazer.local voyager.local" = {
-        identityAgent = "none";
-        extraOptions = {
-          AddKeysToAgent = "no";
-          IdentitiesOnly = "yes";
+  programs = {
+    git = {
+      signing = {
+        format = "ssh";
+        key = "~/.ssh/yubikey_5c_nano";
+        signByDefault = true;
+      };
+      settings = {
+        credential = {
+          helper = "!gh auth git-credential";
+          "https://github.com".username = "ananthb";
         };
       };
-      "*" = {
-        identityFile = "~/.ssh/yubikey_5c_nano";
+    };
+
+    ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "endeavour.local 10.15.16.123 stargazer.local voyager.local" = {
+          identityAgent = "none";
+          extraOptions = {
+            AddKeysToAgent = "no";
+            IdentitiesOnly = "yes";
+          };
+        };
+        "*" = {
+          identityFile = "~/.ssh/yubikey_5c_nano";
+        };
       };
     };
   };
