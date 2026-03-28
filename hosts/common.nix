def6ff3dfc4331384f82fa5a5f687d55e58e387f
Ananth Bhaskararaman <antsub@gmail.com>
2026-03-28 18:06:50 +0530

    add garnix cache

--- hosts/common.nix
+++ hosts/common.nix
@@ -33,6 +33,13 @@
     settings = {
       experimental-features = "nix-command flakes";
 
+      extra-substituters = [
+        "https://cache.garnix.io"
+      ];
+      extra-trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+
       trusted-users = [
         "root"
         username
